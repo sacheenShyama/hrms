accfa79eafa2558cdfc114addf0dc648e5f75eaa
Sachin kumar <93372997+sacheenShyama@users.noreply.github.com>
2025-08-11 04:55:24 +0530

    removed commented code

--- my-app/src/routes/AppRouter.jsx
+++ my-app/src/routes/AppRouter.jsx
@@ -9,40 +9,14 @@ import Profile from "../pages/Profile";
 import Sidebar from "../components/Sidebar";
 import Layout from "../components/Layout";
 import Signup from "../pages/Signup";
-// import { useDispatch } from "react-redux";
-// import {
-//   handleGoogleRedirectResult,
-//   listenForAuthChanges,
-// } from "../redux/service/firebaseAuth";
 
 const AppRouter = () => {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   handleGoogleRedirectResult();
-  //   listenForAuthChanges();
-  // }, []);
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />
       <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      {/* <Sidebar> */}
       <Route element={<Layout />}>
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-        {/* <Route path="/employee" element={<Employees />} />
-        <Route path="/leave-request" element={<LeaveRequest />} />
-        <Route path="/profile" element={<Profile />} />{" "} */}
-        {/* </Sidebar> */}
-        {/* <Route
-        path="/"
-        element={
-          <PrivateRoute>
-            {" "}
-            <Dashboard />{" "}
-          </PrivateRoute>
-        }
-      /> */}
         <Route
           path="/employee"
           element={
